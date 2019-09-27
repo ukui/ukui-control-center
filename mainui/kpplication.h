00988beb61bf75ec1c6427666c52aba57d996bdd
smiley286 <hebing@kylinos.cn>
2019-09-27 09:51:08 +0800

    [hebing]  * 工程中.cpp .h文件增加copyright(gpl2)

--- mainui/kpplication.h
+++ mainui/kpplication.h
@@ -1,12 +1,11 @@
-/*
- * Copyright (C) 2013 ~ 2017 National University of Defense Technology(NUDT) & Tianjin Kylin Ltd.
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * Authors:
- *  Kobe Lee    xiangli@ubuntukylin.com/kobe24_lixiang@126.com
+ * Copyright (C) 2019 Tianjin KYLIN Information Technology Co., Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3.
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,9 +13,10 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
  */
-
 #ifndef KPPLICATION_H
 #define KPPLICATION_H
 
