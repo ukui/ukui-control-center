96ef406c8f9101325224aef57134e69fb8c64eab
smiley286 <hebing@tj.kylinos.cn>
2020-03-21 10:21:33 +0800

    [hebing]  * change org.mate.peripherals-mouse to org.ukui.peripherals-mouse  * fix the crash when change bg type if do not select the picture

--- plugins/personalized/wallpaper/wallpaper.cpp
+++ plugins/personalized/wallpaper/wallpaper.cpp
@@ -185,12 +185,11 @@ void Wallpaper::setupComponent(){
         if (index != _getCurrentBgForm()){
             if (PICTURE == index){
                 //设置图片背景
+                ui->listWidget->setCurrentItem(ui->listWidget->item(0));
 
                 QString fileName = ui->listWidget->currentItem()->data(Qt::UserRole).toString();
 
                 bgsettings->set(FILENAME, fileName);
-                ui->listWidget->setCurrentItem(ui->listWidget->item(0));
-
 
             } else if (COLOR == index){
                 //设置图片背景为空
@@ -475,8 +474,6 @@ void Wallpaper::picWallpaperChangedSlot(QListWidgetItem * current, QListWidgetIt
 
 //    QWidget * currentWidget = ui->listWidget->itemWidget(current);
 //    currentWidget->setStyleSheet("QWidget{border: 5px solid #daebff}");
-
-
 
     QString filename = current->data(Qt::UserRole).toString();
     bgsettings->set(FILENAME, QVariant(filename));
