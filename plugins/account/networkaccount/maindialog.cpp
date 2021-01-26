4f22ac8d959c6d231455ead39f3be1f9b5643d73
David Peng <pengdaixin@kylinos.cn>
2021-01-26 11:48:12 +0800

    Fix:remove qdebug info
    Signed-off-by: David Peng <pengdaixin@kylinos.cn>

--- plugins/account/networkaccount/maindialog.cpp
+++ plugins/account/networkaccount/maindialog.cpp
@@ -547,8 +547,6 @@ void MainDialog::on_timer_timeout() {
 
 /* 登录回调槽函数，登录回执消息后执行此处 */
 void MainDialog::on_login_finished(int ret) {
-    qDebug() << "ssssssssssssssss2";
-    qDebug()<<ret;
     //无手机号码绑定，进入手机号码绑定页面
     //登录返回成功，执行此处
     if(ret == 0) {
