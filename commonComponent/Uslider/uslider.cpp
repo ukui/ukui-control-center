ff53a1c3a7adfdc812c16f3e01e09a8c126b0548
datyuesh <yueshuai@kylinos.cn>
2021-01-30 10:58:51 +0800

    Fix: Screensaver uslider
    Signed-off-by: datyuesh <yueshuai@kylinos.cn>

--- commonComponent/Uslider/uslider.cpp
+++ commonComponent/Uslider/uslider.cpp
@@ -15,36 +15,35 @@ Uslider::Uslider(QStringList list) : scaleList(list), QSlider(Qt::Horizontal)
 
 void Uslider::paintEvent(QPaintEvent *e)
 {
-
     QSlider::paintEvent(e);
 
     auto painter = new QPainter(this);
     painter->setBrush(QBrush(QColor(QPalette::Base)));
 
     auto rect = this->geometry();
-
-    int numTicks = (maximum() - minimum())/tickInterval();
-
+    int numTicks = (maximum() - minimum()) / tickInterval();
+    int total = 0;
     QFontMetrics fontMetrics = QFontMetrics(this->font());
+
+    for (int i=0; i <= numTicks; i++) {
+        QRect fontRect = fontMetrics.boundingRect(scaleList.at(i));
+        total += fontRect.width();
+    }
+    const int interval = (rect.width() - total) / numTicks;
 
     if (this->orientation() == Qt::Horizontal) {
         int fontHeight = fontMetrics.height();
+        int tickX = 1;
+        int tickY = rect.height() / 2 + fontHeight + 5;
+        for (int i=0; i <= numTicks; i++) {
+            QRect fontRect = fontMetrics.boundingRect(scaleList.at(i));
 
-        for (int i=0; i<=numTicks; i++) {
-            int tickNum = minimum() + (tickInterval() * i);
-
-            int tickX = (((rect.width()/numTicks) * i) - (fontMetrics.width(QString::number(tickNum *10))/2 + 2));
-            int tickY = rect.height()/2 + fontHeight + 5;
-
-            if( 0 == i) {
-                tickX += 11;
-            } else if (numTicks == i){
-                tickX -= 10;
-            }
             painter->drawText(QPoint(tickX, tickY),
-                                 this->scaleList.at(i));
-           }
-       }
+                              this->scaleList.at(i));
+            tickX += fontRect.width();
+            tickX += interval;
+        }
+    }
     painter->end();
 }
 
