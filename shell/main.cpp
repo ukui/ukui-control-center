8ad45bb4b212ab430ecb5b5b21b5629333402542
Z-bin <1134636325@qq.com>
2021-03-09 16:47:00 +0800

    feat(log): add log processing
    Description: add log processing
    
    Log: 添加日志处理

--- shell/main.cpp
+++ shell/main.cpp
@@ -24,11 +24,12 @@
 #include <QTranslator>
 #include <QObject>
 #include <QDebug>
-#include <QGSettings>
 #include <QSharedPointer>
 #include <memory>
 #include <QCommandLineParser>
 #include <QQuickWidget>
+#include <QStandardPaths>
+#include <QTime>
 
 #include <stdlib.h>
 
@@ -39,8 +40,52 @@
 
 const QString KLong = "Loongson";
 
+void messageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    QByteArray currentTime = QTime::currentTime().toString().toLocal8Bit();
+
+    bool showDebug = true;
+    QString logFilePath = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "/ukui-control-center.log";
+    if (!QFile::exists(logFilePath)) {
+        showDebug = false;
+    }
+    FILE *log_file = nullptr;
+
+    if (showDebug) {
+        log_file = fopen(logFilePath.toLocal8Bit().constData(), "a+");
+    }
+
+    const char *file = context.file ? context.file : "";
+    const char *function = context.function ? context.function : "";
+    switch (type) {
+    case QtDebugMsg:
+        if (!log_file) {
+            break;
+        }
+        fprintf(log_file, "Debug: %s: %s (%s:%u, %s)\n", currentTime.constData(), localMsg.constData(), file, context.line, function);
+        break;
+    case QtInfoMsg:
+        fprintf(log_file? log_file: stdout, "Info: %s: %s (%s:%u, %s)\n", currentTime.constData(), localMsg.constData(), file, context.line, function);
+        break;
+    case QtWarningMsg:
+        fprintf(log_file? log_file: stderr, "Warning: %s: %s (%s:%u, %s)\n", currentTime.constData(), localMsg.constData(), file, context.line, function);
+        break;
+    case QtCriticalMsg:
+        fprintf(log_file? log_file: stderr, "Critical: %s: %s (%s:%u, %s)\n", currentTime.constData(), localMsg.constData(), file, context.line, function);
+        break;
+    case QtFatalMsg:
+        fprintf(log_file? log_file: stderr, "Fatal: %s: %s (%s:%u, %s)\n", currentTime.constData(), localMsg.constData(), file, context.line, function);
+        break;
+    }
+
+    if (log_file)
+        fclose(log_file);
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(messageOutput);
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
