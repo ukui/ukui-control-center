29266dbebc0e6e5a27b3e371a8215fcfc7a134ec
Peng Daixin <pengdaixin@kylinos.cn>
2021-03-23 18:42:10 +0800

    Fix:need 2 clicks to make switch on/off
    Signed-off-by: Peng Daixin <pengdaixin@kylinos.cn>

--- plugins/account/networkaccount/mainwidget.cpp
+++ plugins/account/networkaccount/mainwidget.cpp
@@ -68,8 +68,8 @@ MainWidget::MainWidget(QWidget *parent) : QWidget(parent) {
     m_szUuid = QUuid::createUuid().toString();
     m_bTokenValid = false;
 
-
-    if (isNetWorkOnline() == false) {
+    m_bIsOnline = isNetWorkOnline();
+    if (m_bIsOnline == false) {
         if (m_autoSyn->get_swbtn()->get_active() == true) {
             m_autoSyn->get_swbtn()->set_active(false);
             for (int i = 0;i < m_szItemlist.size(); i ++ ) {
@@ -106,6 +106,7 @@ void MainWidget::checkNetWork(QVariantMap map) {
         return ;
     }
     if (ret.toInt() != 1 && ret.toInt() != 3 ) {
+        m_bIsOnline = true;
         if (m_autoSyn->get_swbtn()->get_active() == false) {
             m_autoSyn->get_swbtn()->set_active(true);
             for (int i = 0;i < m_szItemlist.size(); i ++ ) {
@@ -121,6 +122,7 @@ void MainWidget::checkNetWork(QVariantMap map) {
 
         return ;
     }
+    m_bIsOnline = false;
     if (m_autoSyn->get_swbtn()->get_active() == true) {
         m_autoSyn->get_swbtn()->set_active(false);
         for (int i = 0;i < m_szItemlist.size(); i ++ ) {
@@ -961,7 +963,7 @@ void MainWidget::on_switch_button(int on,int id) {
     if (m_mainWidget->currentWidget() == m_nullWidget) {
         return ;
     }
-    if (isNetWorkOnline() == false) {
+    if (m_bIsOnline == false) {
         showDesktopNotify(tr("Network can not reach!"));
         return ;
     }
