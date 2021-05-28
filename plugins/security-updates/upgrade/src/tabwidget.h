557c43db81771376d9971eb5628eec93efc94116
YouYuFei <1358717030@qq.com>
2021-05-28 17:37:35 +0800

    解决控制面板卡顿问题

--- plugins/security-updates/upgrade/src/tabwidget.h
+++ plugins/security-updates/upgrade/src/tabwidget.h
@@ -126,7 +126,6 @@ public slots:
 
 private:
     UKSCConn *ukscConnect;
-    bool firstCheckedStatus = false;
     bool isConnectSourceSignal = false;
     void unableToConnectSource();
 //备份还原相关
@@ -148,6 +147,8 @@ public slots:
     void isAutoBackupChanged();
 
     void getReplyFalseSlot();
+    
+    void dbusFinished();
 
 private slots:
     void receiveBackupStartResult(int result);
