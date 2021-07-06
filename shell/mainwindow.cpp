dc360b82eb7505bc3bd257c629db316ab37f5258
supreme886 <985636263@qq.com>
2021-07-06 13:55:12 +0800

    Fixed:bug#44725

--- shell/mainwindow.cpp
+++ shell/mainwindow.cpp
@@ -813,7 +813,8 @@ bool MainWindow::isExitBluetooth() {
     if (QGSettings::isSchemaInstalled(bluetoothId)) {
         QGSettings bluetoothGSetting(bluetoothId);
         isAddress = bluetoothGSetting.get("adapter-address").toString().isEmpty() ? false : true;
-    }
+    } else
+        isAddress = false;
 
     return isDevice && isAddress;
 }
