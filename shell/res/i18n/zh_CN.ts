6e03f94c7d31be73fd23ac7a671b47ad17924400
Your Name <you@example.com>
2021-07-06 14:51:05 +0800

    完成需求6006

--- shell/res/i18n/zh_CN.ts
+++ shell/res/i18n/zh_CN.ts
@@ -7812,7 +7812,7 @@ run start-pulseaudio-x11 manually.</source>
     <message>
         <location filename="../../../plugins/devices/audio/ukmedia_sound_effects_widget.cpp" line="59"/>
         <source>Alert Sound</source>
-        <translation>通知音</translation>
+        <translation>通知提示</translation>
         <extra-contents_path>/audio/Alert Sound</extra-contents_path>
     </message>
     <message>
@@ -7862,7 +7862,7 @@ run start-pulseaudio-x11 manually.</source>
     <message>
         <location filename="../../../plugins/devices/audio/ukmedia_sound_effects_widget.cpp" line="84"/>
         <source>Volume Change</source>
-        <translation>音量调节声</translation>
+        <translation>音量调节</translation>
     </message>
     <message>
         <source>Setting Menu</source>
