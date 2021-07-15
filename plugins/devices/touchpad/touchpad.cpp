0f0a485ce383eadfaeb2888c3f47db7bd39ac214
lan <873174907@qq.com>
2021-07-15 17:11:10 +0800

    fix(Touchpad): Touchpad
    Description:  Fix Touchpad Question
    
    Log: 【控制面板|触摸板】设置插入鼠标时候禁用触摸板不生效

--- plugins/devices/touchpad/touchpad.cpp
+++ plugins/devices/touchpad/touchpad.cpp
@@ -204,7 +204,7 @@ void Touchpad::initTouchpadStatus(){
 
     // 插入鼠标时候禁用触摸板
     mMouseDisTouchBtn->blockSignals(true);
-    clickBtn->setChecked(tpsettings->get(MOUSE_DISBALE_TOUCHPAD).toBool());
+    mMouseDisTouchBtn->setChecked(tpsettings->get(MOUSE_DISBALE_TOUCHPAD).toBool());
     mMouseDisTouchBtn->blockSignals(false);
 
     //初始化滚动
