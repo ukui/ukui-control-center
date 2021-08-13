4c920fb616d7da56cdf303b4bfeca3bcbfbb8550
lan <873174907@qq.com>
2021-08-13 16:28:17 +0800

    fix(Power): Power
    Description: Power
    
    Log: 判断是否存在ukui-power-manager

--- shell/mainwindow.cpp
+++ shell/mainwindow.cpp
@@ -746,9 +746,15 @@ bool MainWindow::isExitsCloudAccount() {
 
 bool MainWindow::isExitsPower()
 {
-    QProcess *process = new QProcess(this);
-    process->start("ukui-power-manager",NULL);
-    return process->waitForStarted() ? true : false;
+    QProcess *process = new QProcess;
+    process->start("dpkg -l ukui-power-manager");
+    process->waitForFinished();
+
+    QByteArray ba = process->readAllStandardOutput();
+    delete process;
+    QString mOutput = QString(ba.data());
+
+    return mOutput.contains("ii", Qt::CaseSensitive) ? true : false;
 }
 
 bool MainWindow::dblOnEdge(QMouseEvent *event) {
