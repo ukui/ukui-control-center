99b35c2924b1d25013d3e79986ecaf0f5f4dc076
Peng Daixin <pengdaixin@kylinos.cn>
2021-08-23 13:56:21 +0800

    Fix(cloud account):bug #71819 #71254
    Signed-off-by: Peng Daixin <pengdaixin@kylinos.cn>

--- plugins/account/networkaccount/mainwidget.cpp
+++ plugins/account/networkaccount/mainwidget.cpp
@@ -431,10 +431,15 @@ void MainWidget::refreshSyncDate() {
     QVariant ret = ConfigFile(m_szConfPath).Get("Auto-sync","time");
     if (m_pSettings != nullptr && fileConf.exists() && fileConf.size() > 1 && !ret.isNull()) {
         m_syncTimeLabel->setText(tr("The latest time sync is: ") +   ret.toString().toStdString().c_str());
+        m_syncTimeLabel->show();
     }
-    else {
+    else if (m_pSettings != nullptr && fileConf.exists() && fileConf.size() > 1){
+        emit isSync(false);
+        m_syncTimeLabel->hide();
+    } else {
         emit isSync(true);
         m_syncTimeLabel->setText(tr("Waiting for initialization..."));
+        m_syncTimeLabel->show();
     }
 }
 
@@ -511,6 +516,7 @@ void MainWidget::initMemoryAlloc() {
     m_welcomeLayout = new QVBoxLayout;
     m_welcomeImage = new QSvgWidget(":/new/image/96_color.svg");
     m_welcomeMsg = new TitleLabel(this);
+    //~ contents_path /networkaccount/Sign in
     m_login_btn  = new QPushButton(tr("Sign in"),this);
     m_svgHandler = new SVGHandler(this);
     m_stackedWidget = new QStackedWidget(this);
