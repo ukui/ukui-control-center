687ddfdc167dd8aa6635c6bd4b281d2b90ff9576
Z-bin <1134636325@qq.com>
2021-09-15 14:01:33 +0800

    fix: Wrong font size range
    Description: Wrong font size range
    
    Log: 调整字体大小范围
    Bug: http://pm.kylin.com/biz/bug-view-78529.html

--- plugins/personalized/fonts/fonts.cpp
+++ plugins/personalized/fonts/fonts.cpp
@@ -138,8 +138,8 @@ void Fonts::setupStylesheet(){
 void Fonts::setupComponent(){
     ui->resetBtn->setVisible(false);
     QStringList fontScale;
-    fontScale<< "11pt" << "12pt" << "13pt" << "14pt" << "15pt"
-              <<"16pt";
+    fontScale<< "10" << "11" << "12" << "13" << "14"
+              <<"15";
 
     uslider  = new Uslider(fontScale);
     uslider->setRange(1,6);
@@ -247,28 +247,28 @@ QStringList Fonts::_splitFontNameSize(QString value) {
 
 int Fonts::fontConvertToSlider(const int size) const {
     switch (size) {
-    case 11:
+    case 10:
         return 1;
         break;
-    case 12:
+    case 11:
         return 2;
         break;
-    case 13:
+    case 12:
         return 3;
         break;
-    case 14:
+    case 13:
         return 4;
         break;
-    case 15:
+    case 14:
         return 5;
         break;
-    case 16:
+    case 15:
         return 6;
         break;
-    case 17:
+    case 16:
         return 7;
         break;
-    case 18:
+    case 17:
         return 8;
         break;
     default:
@@ -281,30 +281,30 @@ int Fonts::sliderConvertToSize(const int value) const
 {
     switch (value) {
     case 1:
-        return 11;
+        return 10;
         break;
     case 2:
-        return 12;
+        return 11;
         break;
     case 3:
-        return 13;
+        return 12;
         break;
     case 4:
-        return 14;
+        return 13;
         break;
     case 5:
-        return 15;
+        return 14;
         break;
     case 6:
-        return 16;
+        return 15;
         break;
     case 7:
-        return 17;
+        return 16;
         break;
     case 8:
-        return 18;
+        return 17;
     default:
-        return 11;
+        return 10;
         break;
     }
 }
