7d4aeb56e1fdccf9922c0e2b2f1bd6324466bb01
fengChao <834317452@qq.com>
2021-09-16 09:04:33 +0800

    fix(shortcut): fix bug#81218
    Description: 【UKUI V3.1】【控制面板】部分自定义快捷键名称（目前已知s、c结尾的名称）超过最大长度后显示的省略号是两个点，建议均显示为三个点
    
    Log: label文字宽度问题
    Bug: http://172.17.66.192/biz/bug-view-81218.html

--- plugins/devices/shortcut/clickfixlabel.cpp
+++ plugins/devices/shortcut/clickfixlabel.cpp
@@ -2,7 +2,7 @@
 #include <QDebug>
 
 ClickFixLabel::ClickFixLabel(QWidget *parent):
-    FixLabel(parent)
+    QLabel(parent)
 {
     this->setContentsMargins(8,0,0,0);
 }
@@ -17,4 +17,26 @@ void ClickFixLabel::mouseDoubleClickEvent(QMouseEvent *event)
     Q_UNUSED(event);
     Q_EMIT doubleClicked();
     return;
+}
+
+void ClickFixLabel::paintEvent(QPaintEvent *event)
+{
+    QFontMetrics fontMetrics(this->font());
+    int fontSize = fontMetrics.width(mStr);
+    int m_width = this->width() - 8;
+    if (fontSize > m_width) {
+        this->setText(fontMetrics.elidedText(mStr, Qt::ElideRight, m_width), false);
+        this->setToolTip(mStr);
+    } else {
+        this->setText(mStr, false);
+        this->setToolTip("");
+    }
+    QLabel::paintEvent(event);
+}
+
+void ClickFixLabel::setText(const QString & text, bool saveTextFlag)
+{
+    if (saveTextFlag)
+        mStr = text;
+    QLabel::setText(text);
 }
