a04b6ac4704f66fdb3207b7729b5beafbc8c091c
fengChao <834317452@qq.com>
2021-10-15 11:35:51 +0800

    fix(wallpaper): fix bug#85016
    Description: 【设置|背景】选择颜色壁纸后，图片中的原壁纸还是选中状态
    
    Log: 选择颜色背景后去掉图片的选中效果
    Bug: http://pm.kylin.com/biz/bug-view-85016.html

--- shell/mainwindow.h
+++ shell/mainwindow.h
@@ -70,7 +70,7 @@ private:
 
 private:
     HomePageWidget * homepageWidget;
-    ModulePageWidget * modulepageWidget;
+    ModulePageWidget * modulepageWidget = nullptr;
 
     QButtonGroup * leftBtnGroup;
     QButtonGroup * leftMicBtnGroup; //
