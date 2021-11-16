c11e7e48bfafc197964549991172422737410415
Z-bin <1134636325@qq.com>
2021-11-16 17:41:58 +0800

    fix:bug#90457:修改分辨率重疊问题

--- plugins/system/display/outputconfig.cpp
+++ plugins/system/display/outputconfig.cpp
@@ -328,9 +328,7 @@ void OutputConfig::slotResolutionChanged(const QSize &size, bool emitFlag)
 
     if (!mIsModeInit) {
         mIsRestore = false;
-        mOutput->blockSignals(true); //避免修改分辨率缩略图多次变化
         mOutput->setCurrentModeId(modeID);
-        mOutput->blockSignals(false);
         if (emitFlag) {
             changeItm = RESOLUTION;
             Q_EMIT changed();
