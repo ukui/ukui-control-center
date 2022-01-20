366b8ffae4568871049db0d3e0c92513b538fba6
hesisheng <hesisheng@kylinos.cn>
2022-01-20 15:18:33 +0800

    fix(userinfo): usericon
    Description: default face while creatusernew
    
    Log: 创建用户时的默认头像根据iconfile定制，控制面板不设置为default.png
    Task: https://172.17.50.104/biz/task-view-66958.html

--- registeredQDbus/sysdbusregister.cpp
+++ registeredQDbus/sysdbusregister.cpp
@@ -246,12 +246,16 @@ int SysdbusRegister::createUser(QString name, QString fullname, int accounttype,
         QString op = reply.value().path();
         if (!op.isEmpty()){
 
-            QDBusInterface ifaceUser("org.freedesktop.Accounts",
+            QDBusInterface iproperty("org.freedesktop.Accounts",
                                      op,
                                      "org.freedesktop.DBus.Properties",
                                      QDBusConnection::systemBus());
+            QDBusInterface ifaceUser("org.freedesktop.Accounts",
+                                     op,
+                                     "org.freedesktop.Accounts.User",
+                                     QDBusConnection::systemBus());
             // 设置头像, 默认头像不存在则设置DEFAULTFACE
-            QDBusReply<QMap<QString, QVariant> > user = ifaceUser.call("GetAll", "org.freedesktop.Accounts.User");
+            QDBusReply<QMap<QString, QVariant> > user = iproperty.call("GetAll", "org.freedesktop.Accounts.User");
             char *iconpath = user.value().find("IconFile").value().toString().toLatin1().data();
             if (!g_file_test(iconpath, G_FILE_TEST_EXISTS)){
                 ifaceUser.call("SetIconFile", faceicon);
