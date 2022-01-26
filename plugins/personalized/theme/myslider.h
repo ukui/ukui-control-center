2630b39cd5acdcec1f4d6dbe748f6bcb96af82aa
lan <873174907@qq.com>
2022-01-26 16:32:55 +0800

    fix(Theme): ThemeTheme
    Description: Theme
    
    Log: 修改滑动条

--- plugins/personalized/theme/myslider.h
+++ plugins/personalized/theme/myslider.h
@@ -9,7 +9,13 @@ class MySlider : public QSlider
 public:
     MySlider(QWidget *parent = nullptr);
     ~MySlider();
+protected:
     void mousePressEvent(QMouseEvent *ev);
+    void wheelEvent(QWheelEvent *ev);
+    void leaveEvent(QEvent *ev);
+
+private:
+    bool isMouseCliked = false;
 
 };
 
