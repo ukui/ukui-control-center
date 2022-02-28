01a43a1af8dfddeafc4f4e15722acd1c3181d65e
lan <873174907@qq.com>
2022-02-28 15:22:56 +0800

    fix(About): About
    Description: About
    
    Log:  【关于麒麟】页隐藏服务有效时间，方法为点击序列号后显示服务到期时间等内容
    Task: https://172.17.50.104/biz/story-view-11566.html

---
+++ plugins/messages-task/about/statusdialog.cpp
@@ -0,0 +1,103 @@
+#include "statusdialog.h"
+#include <QVBoxLayout>
+
+StatusDialog::StatusDialog(QWidget *parent) :
+    QDialog(parent)
+{
+    this->setWindowFlags(Qt::Dialog);
+    setWindowTitle(tr("About"));
+    initUI();
+}
+
+StatusDialog::~StatusDialog()
+{
+
+}
+
+void StatusDialog::initUI()
+{
+    this->setFixedSize(450 , 384);
+    QVBoxLayout *vLyt = new QVBoxLayout(this);
+    vLyt->setContentsMargins(24 , 16 , 24 , 40);
+    vLyt->setSpacing(8);
+
+    mLogoLabel = new QLabel(this);
+
+    // 版本号
+    mVersionFrame = new QFrame(this);
+    QHBoxLayout *mVersionLyt = new QHBoxLayout(mVersionFrame);
+    mVersionLyt->setSpacing(16);
+    mVersionFrame->setFixedHeight(36);
+    mVersionFrame->setFrameShape(QFrame::NoFrame);
+    mVersionLabel_1 = new QLabel(mVersionFrame);
+    mVersionLabel_2 = new QLabel(mVersionFrame);
+    mVersionLabel_1->setFixedWidth(100);
+    mVersionLyt->addWidget(mVersionLabel_1);
+    mVersionLyt->addWidget(mVersionLabel_2);
+
+    // 系统状态
+    mStatusFrame = new QFrame(this);
+    QHBoxLayout *mStatusLyt = new QHBoxLayout(mStatusFrame);
+    mStatusLyt->setSpacing(16);
+    mStatusFrame->setFixedHeight(36);
+    mStatusFrame->setFrameShape(QFrame::NoFrame);
+    mStatusLabel_1 = new QLabel(mStatusFrame);
+    mStatusLabel_2 = new QLabel(mStatusFrame);
+    mStatusLabel_1->setFixedWidth(100);
+    mStatusLyt->addWidget(mStatusLabel_1);
+    mStatusLyt->addWidget(mStatusLabel_2);
+
+    // 序列号
+    mSerialFrame = new QFrame(this);
+    QHBoxLayout *mSerialLyt = new QHBoxLayout(mSerialFrame);
+    mSerialLyt->setSpacing(16);
+    mSerialFrame->setFixedHeight(36);
+    mSerialFrame->setFrameShape(QFrame::NoFrame);
+    mSerialLabel_1 = new QLabel(mSerialFrame);
+    mSerialLabel_2 = new QLabel(mSerialFrame);
+    mSerialLabel_1->setFixedWidth(100);
+    mSerialLyt->addWidget(mSerialLabel_1);
+    mSerialLyt->addWidget(mSerialLabel_2);
+
+    // 服务到期时间
+    mTimeFrame = new QFrame(this);
+    QHBoxLayout *mTimeLyt = new QHBoxLayout(mTimeFrame);
+    mTimeLyt->setSpacing(16);
+    mTimeFrame->setFixedHeight(36);
+    mTimeFrame->setFrameShape(QFrame::NoFrame);
+    mTimeLabel_1 = new QLabel(mTimeFrame);
+    mTimeLabel_2 = new QLabel(mTimeFrame);
+    mTimeLabel_1->setFixedWidth(100);
+    mTimeLyt->addWidget(mTimeLabel_1);
+    mTimeLyt->addWidget(mTimeLabel_2);
+
+    mExtentBtn = new QPushButton(this);
+    mExtentBtn->setFixedHeight(40);
+
+    vLyt->addWidget(mLogoLabel);
+    vLyt->addSpacing(24);
+    vLyt->addWidget(mVersionFrame);
+    vLyt->addWidget(mStatusFrame);
+    vLyt->addWidget(mSerialFrame);
+    vLyt->addWidget(mTimeFrame);
+    vLyt->addStretch();
+    vLyt->addWidget(mExtentBtn);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
