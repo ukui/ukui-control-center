40537e49b0157042035744d5bfb4356b18a3c4bc
lan <873174907@qq.com>
2022-04-21 16:16:07 +0800

    fix(DateTime Zone): DateTime Zone
    Description: DateTime Zone
    
    Log: 【控制面板|时间和日期】深色主题下修改时区多个城市选择弹框未深色处理
    Bug: https://172.17.50.104/biz/bug-view-114186.html

--- plugins/time-language/datetime/worldMap/poplistdelegate.cpp
+++ plugins/time-language/datetime/worldMap/poplistdelegate.cpp
@@ -9,21 +9,28 @@ PopListDelegate::PopListDelegate(QWidget* parent) : QStyledItemDelegate (parent)
 void PopListDelegate::paint(QPainter *painter, const QStyleOptionViewItem& option,const QModelIndex& index) const{
     painter->save();
 
-    QRect rect(option.rect);
+    painter->setRenderHint(QPainter::Antialiasing);  // 反锯齿;
+   painter->setPen(Qt::transparent);
+   QRect rect(option.rect);
+   rect.setWidth(rect.width() - 1);
+   rect.setHeight(rect.height() - 1);
+
 
     if(option.state.testFlag(QStyle::State_MouseOver)) {
         QColor hoverColor(44, 167, 248);
-        painter->fillRect(rect, QBrush(hoverColor));
+        painter->setBrush(QBrush(hoverColor));
+        painter->drawRoundedRect(rect, 4, 4);
     }
 
-    QColor textColor(48, 48, 48);
+    QPalette pal;
+    QBrush brush = pal.buttonText();
+    QColor textColor = brush.color();
     if(option.state.testFlag(QStyle::State_MouseOver)) {
         textColor = Qt::white;
     }
 
     painter->setPen(QPen(textColor));
     QString text = index.model()->data(index, Qt::DisplayRole).toString();
-
-      painter->drawText(rect, Qt::AlignHCenter | Qt::AlignVCenter, text);
+    painter->drawText(QRect(rect.x() + 8,rect.y(),rect.width(),rect.height()), Qt::AlignLeft | Qt::AlignVCenter, text);
     painter->restore();
 }
